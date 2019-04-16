4a3f4279d5c548442625f34b726ce3fe1e6b1b30
Christian von Lehe <kring.vonlehe@gmail.com>
2019-04-15 21:37:59 -0400

    added: boundary logic

--- src/Game/index.js
+++ src/Game/index.js
@@ -6,6 +6,7 @@ class Game extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      gameStatus: '',
       position: {x: 0, y: 0},
     }
   }
@@ -31,26 +32,40 @@ class Game extends Component {
     const robot = document.getElementById('robot');
 
     for(let i = 0; i < movesArr.length; i++) {
-      setTimeout(function timer(){
+      setTimeout(() => {
 
         if(movesArr[i] === 'up') {
           position.y -= 1
-          console.log(position)
+          // console.log(position)
         } else if(movesArr[i] === 'down') {
           position.y += 1
-          console.log(position)
+          // console.log(position)
         } else if(movesArr[i] === 'left') {
           position.x -= 1
-          console.log(position)
+          // console.log(position)
         } else  {
           position.x += 1
-          console.log(position)
+          // console.log(position)
         }
 
+        this.checkMove(position) ?
         // position the robot based off the commands
-        robot.style.transform = `translate(${position.x * 120}px, ${position.y * 120}px)`;
-        // console.log('transform')
+        robot.style.transform = `translate(${position.x * 120}px, ${position.y * 120}px)` :
+
+        // robot fall
+        robot.style.transform = `translateY(110vh)`;
+
       }, i*750 );
+    }
+  }
+
+  checkMove(position) {
+    if(position.x < 0 || position.y < 0) {
+      return false
+    } else if(position.x > 4 || position.y > 4) {
+      return false
+    } else {
+      return true
     }
   }
 
